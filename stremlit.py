76db1a54c5aeff100de041e9d7914757deb1f8e4
Balanagireddy Vallem <53854911+Balu2311@users.noreply.github.com>
2024-10-25 23:54:54 +0530

    Update stremlit.py

--- stremlit.py
+++ stremlit.py
@@ -2,8 +2,7 @@ import streamlit as st
 import requests
 
 st.title("Streamlit and Flask Integration")
-
 if st.button("Fetch Data"):
-    response = requests.get('http://localhost:5000/api/data')
+    response = requests.get('http://127.0.0.1:5001')
     data = response.json()
     st.write(data)
